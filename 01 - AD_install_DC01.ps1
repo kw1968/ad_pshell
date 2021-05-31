e329b64a222dd40722983333f8ae692dc54f92a7
Kurt Windberger <82037622+kw1968@users.noreply.github.com>
2021-05-31 09:07:47 +0200

    Update 01 - AD_install_DC01.ps1

--- 01 - AD_install_DC01.ps1
+++ 01 - AD_install_DC01.ps1
@@ -1,2 +1,4 @@
 # 1. Schritt: AD Dienste installieren; danach tauchen AD Einträge in Serververwaltung auf mit Ausrufezeichen
+# Installiert werden die AD Dienste zusammen mit den Verwaltungswerkzeugen (GPO, AD BEnutzer, Vertrauensstelllungen) etc.
+# NAch dem Neustart müssen diese Dienste konfiguriert werden (Serververwaltung im Dashboard)
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
