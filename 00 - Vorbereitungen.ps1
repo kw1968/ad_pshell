1143e9b56d66cf2a9ef4e1492c293efe37b65ecf
Kurt Windberger <82037622+kw1968@users.noreply.github.com>
2021-06-01 09:49:02 +0200

    Update 00 - Vorbereitungen.ps1

--- 00 - Vorbereitungen.ps1
+++ 00 - Vorbereitungen.ps1
@@ -15,3 +15,15 @@ Get-WindowsCapability -Online | ? {$_.Name -like "*RSAT*"} |Add-WindowsCapabilit
 
 # Alle Komponenten deinstallieren; funktioniert in neueren Versionen nicht mehr ( Ein permanentes Paket kann nicht deinstalliert werden.)
 Get-WindowsCapability -Online | ? {$_.Name -like "*RSAT*" -and $_.State -eq "Installed"} | Remove-WindowsCapability -Online
+
+# RSAT Tools auf Server DC01 (10.36.39.11) installieren; DNS NAme geht, wenn im lokalen Netz
+$InstallSB = {
+Get-WindowsFeature -Name *RSAT* | Install-WindowsFeature
+}
+$I1 = Invoke-Command -ComputerName DC01 -ScriptBlock $InstallSB
+$I1
+# Neustart funktioniert eventuell nicht automatisch (-wait ist problematisch); dann halt manuell neu starten
+If ($I1.RestartNeeded -eq 'Yes') {
+"DC01(10.36.39.11) wird neu gestartet"
+Restart-Computer -ComputerName DC01 -Force -Wait -For PowerShell
+}
